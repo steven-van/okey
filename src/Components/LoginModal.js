a3fd06b9d412e1290e632bc38cc301dfdec6293f
steven-van <64859587+steven-van@users.noreply.github.com>
2023-02-21 01:05:00 +0100

    feat: initial commit, login page and mockup cards

---
+++ src/Components/LoginModal.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const LoginModal = () => {
+  return (
+    <div className="w-full max-w-sm p-4 bg-white rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800">
+      <form className="space-y-6" action="#">
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+          Sign in to our platform
+        </h5>
+        <div>
+          <label
+            for="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="name@company.com"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+        <div className="flex items-start">
+          <div className="flex items-start">
+            <div className="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3  dark:bg-gray-700 dark:border-gray-600  dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                required
+              />
+            </div>
+            <label
+              for="remember"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Remember me
+            </label>
+          </div>
+          <a href="#" className="ml-auto text-sm text-red-500 hover:underline">
+            Lost Password?
+          </a>
+        </div>
+        <button
+          type="submit"
+          className="w-full text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Login to your account
+        </button>
+        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+          Not registered?{" "}
+          <a href="#" className="text-red-500 hover:underline">
+            Create account
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginModal;
---
+++ src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginModal from "../Components/LoginModal";
+
+const Login = () => {
+  return (
+    <div className="container bg-gray-100 dark:bg-gray-900">
+      <LoginModal />
+    </div>
+  );
+};
+
+export default Login;
